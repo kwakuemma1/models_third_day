38b6e6fe1c5d9bafb49af9821fc999bf4fcb91b0
kwakuemma1 <emmayeboah2007@gmail.com>
2023-06-29 09:35:20 -0500

    loaded packages and functions

---
+++ demo_thurs.R
@@ -0,0 +1,13 @@
+# To check for packages
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg) #, dependencies = TRUE
+  sapply(pkg, require, character.only = TRUE)
+}
+
+ipak("r4ds.tutorials")
+# Tutorial Packages
+library(r4ds.tutorials)
+library(all.primer.tutorials)
+library(tidycensus.tutorials)
